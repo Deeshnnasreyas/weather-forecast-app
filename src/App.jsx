5f674d09e858e86afad0efa1edb6a4235247f570
deeshnna <deeshnna@adiogrow.com>
2024-11-12 16:00:55 +0530

    updates

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import Forecast from "./components/Forecast";
+import Inputs from "./components/Inputs";
+import TempAndDetails from "./components/TempAndDetails";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TopButtons from "./components/TopButtons";
+import getFormattedWeatherData from "./services/weatherService";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const App = () => {
+  const [query, setQuery] = useState({ q: "ahmedabad" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  const getWeather = async () => {
+    const cityName = query.q ? query.q : "current location";
+    toast.info(`Fetching weather data for ${capitalizeFirstLetter(cityName)}`);
+
+    await getFormattedWeatherData({ ...query, units }).then((data) => {
+      toast.success(`Fetched weather data for ${data.name}, ${data.country}`);
+      setWeather(data);
+    });
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, [query, units]);
+
+  return (
+    <div
+      className={`mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-blue-100 to-blue-700`}
+    >
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} setUnits={setUnits} />
+
+      {weather && (
+        <>
+          <TimeAndLocation weather={weather} />
+          <TempAndDetails weather={weather} units={units} />
+          <Forecast title="3 hour step forecast" data={weather.hourly} />
+          <Forecast title="daily forecast" data={weather.daily} />
+        </>
+      )}
+
+      <ToastContainer autoClose={2500} hideProgressBar={true} theme="colored" />
+    </div>
+  );
+};
+
+export default App;
